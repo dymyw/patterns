89a4914d4000cd236ba8f5324d24254700b20556
dymyw <dymayongwei@163.com>
2021-07-28 17:09:35 +0800

    feat: add php abstract factory

---
+++ PHP/Factory/4.AbstractFactory.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * 组合思想
+ * 优点：增加固定类型产品的不同具体工厂比较方便
+ */
+
+/**
+ * TV 产品规范
+ *
+ * Interface TV
+ */
+interface TV
+{
+    public function open();
+    public function watch();
+}
+
+/**
+ * PC 产品规范
+ *
+ * Interface PC
+ */
+interface PC
+{
+    public function work();
+    public function play();
+}
+
+/**
+ * Phone 产品规范
+ *
+ * Interface Phone
+ */
+interface Phone
+{
+    public function work();
+    public function sms();
+}
+
+/**
+ * AbstractFactory 抽象工厂类
+ *
+ * Class AbstractFactory
+ */
+abstract class AbstractFactory
+{
+    abstract public static function createTV(): TV;
+    abstract public static function createPC(): PC;
+    abstract public static function createPhone(): Phone;
+}
+
+/**
+ * ProductFactory 产品工厂
+ *
+ * Class ProductFactory
+ */
+class ProductFactory extends AbstractFactory
+{
+    public static function createTV(): TV
+    {
+        // ...
+    }
+
+    public static function createPC(): PC
+    {
+        // ...
+    }
+
+    public static function createPhone(): Phone
+    {
+        // ...
+    }
+}
+
+// 使用
+ProductFactory::createPC()->play();
